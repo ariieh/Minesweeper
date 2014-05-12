44e162b5ea4bfb164053242586b5cd03f1274e28
Arieh + Dan <arieh.smith@gmail.com>
2014-05-12 14:52:48 -0400

    Add any/all method and win/lose

--- minesweeper.rb
+++ minesweeper.rb
@@ -18,9 +18,23 @@ class Minesweeper
       @board.uncover(move)
     end
   end
-
-  def over?
-    
+  
+  def win?
+    return true if @board.all? do |row, col|
+      if @board[row, col].value == :mine
+        @board[row, col].flagged?
+      else
+        !@board[row, col].flagged?
+      end
+    end
+    false
+  end
+  
+  def lose?
+    return true if @board.any? do |row, col|
+      @board[row, col].value == :mine && @board[row, col].revealed?
+    end
+    false
   end
   
   def get_input
@@ -61,15 +75,10 @@ class Board
     @board = Array.new(BOARD_SIDE_LENGTH){ Array.new(BOARD_SIDE_LENGTH) }
 
     each { |row, col| @board[row][col] = Tile.new(elements.pop) }
-
-    @board.each_index do |row|
-      @board[row].each_index do |col|
-        
-        neighbors = surrounding_tiles([row, col]).map{|x, y| self[x, y]}
-        nearby_mines = neighbors.select { |tile| tile.value == :mine }.count
-        @board[row][col].num_mines = nearby_mines
-        
-      end
+    each do |row, col|
+      neighbors = surrounding_tiles([row, col]).map{|x, y| self[x, y]}
+      nearby_mines = neighbors.select { |tile| tile.value == :mine }.count
+      @board[row][col].num_mines = nearby_mines
     end
   end
   
@@ -79,6 +88,20 @@ class Board
         prc.call(row, col)
       end
     end
+  end
+  
+  def any?(&prc)
+    each do |row, col|
+      return true if prc.call(row, col)
+    end
+    false
+  end
+  
+  def all?(&prc)
+    each do |row, col|
+      return false unless prc.call(row, col)
+    end
+    true
   end
   
   def print_board
@@ -135,6 +158,10 @@ class Tile
   
   def revealed?
     @revealed
+  end
+  
+  def flagged?
+    @flagged
   end
   
   def flag!
